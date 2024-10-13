46083b8486a7c88c7232fa532f150a3cdf224f6b
Vanessa Zuñiga <van.zunigap@profesor.duoc.cl>
2024-10-13 22:52:32 +0000

    Agregar script para captura de datos personales

---
+++ datos_personales.py
@@ -0,0 +1,11 @@
+# datos_personales.py
+nombre = input("Ingresa tu nombre: ")
+apellido = input("Ingresa tu apellido: ")
+edad = input("Ingresa tu edad: ")
+sede = input("Ingresa tu sede: ")
+
+print("\nDatos personales:")
+print(f"Nombre: {nombre}")
+print(f"Apellido: {apellido}")
+print(f"Edad: {edad}")
+print(f"Sede: {sede}")
